2a55a8465ed8498658f94ebb4ed496d09786f9b2
Weit145 <weit.dima@yandex.ru>
2025-11-24 14:15:13 +0300

    Lab7 OP :fire:

--- 2 curs/OP/lab_7/lab7.cpp
+++ 2 curs/OP/lab_7/lab7.cpp
@@ -1,12 +1,44 @@
 #include "lab7.h"
+#include <cstdlib> 
 #include <iostream>
 
 using namespace std;
 
-int Example::zad(){
-    return 0;
+
+template <typename T>
+void rd(vector<T> & t, const int size){
+    for (int i=0;i<size;i++){
+        double num = (rand() % 100) / 10.0;
+        t[i]=T(num);
+    }
 }
 
 int main(){
-	vector<int> v = {1,2,3,4,5};
+    srand(time(NULL));
+    const int size = 10;
+	vector<int> v(size);
+    rd(v,size);
+    Example V = Example(v,size);
+    cout<<V.zad1()<<endl;
+    V.show();
+    V.zad2();
+    V.show();
+
+
+    vector<double> k(size);
+    rd(k,size);
+    Example K = Example(k,size);
+    cout<<K.zad1()<<endl;
+    K.show();
+    K.zad2();
+    K.show();
+
+
+    vector<float> g(size);
+    rd(g,size);
+    Example G = Example(g,size);
+    cout<<G.zad1()<<endl;
+    G.show();
+    G.zad2();
+    G.show();
 }
--- 2 curs/OP/lab_7/lab7.h
+++ 2 curs/OP/lab_7/lab7.h
@@ -6,8 +6,36 @@ using namespace std;
 template <typename T>
 class Example{
 private:
-    T vec;
+    vector<T> vec;
+    const int size;
 public:
-    Example(const T& t): vec(t) {}
-    int zad();
+    Example(vector<T> & t,const int size): vec(t), size(size) {}
+
+    int zad1(){
+        int sum=0;
+        for(int i : vec){
+            double k = double(i);
+            sum+=k;
+        }
+        return T(sum);   
+    }
+
+    void zad2(){
+        for(int i=0;i<size;i++){
+            double k = double(vec[i]);
+            double val = 2*k+5;
+            if(val>20){
+                vec[i]=T(10);
+                continue;
+            }
+            vec[i]=T(val);
+        }
+    }
+
+    void show(){
+        for(double val : vec){
+            cout << val << " ";
+        }
+        cout << endl;
+    }
 };
