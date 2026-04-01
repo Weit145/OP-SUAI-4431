3958ea5cb1f8376853d93bae162ed1a195bdb2b3
Weit145 <weit.dima@yandex.ru>
2026-04-01 22:11:39 +0300

    Fix :bug:

--- 2 curs/2_half/Func_Log/lab2/lab2.lisp
+++ 2 curs/2_half/Func_Log/lab2/lab2.lisp
@@ -1,59 +1,82 @@
-(defun neighbour2 (v graph)
-  (cond ((null graph) nil)
-        ((eq v (caar graph))
-         (cons (cdar graph) (neighbour2 v (cdr graph))))
-        (t (neighbour2 v (cdr graph)))))
+(defun neighb (v graf)
+  (cond ((null graf) nil)
+        ((eq v (caar graf))
+          (cons (cdar graf) (neighb v (cdr graf))))
+        (t (neighb v (cdr graf)))))
 
-(defun member-eq (item lst)
+(defun mb (i lst)
   (cond ((null lst) nil)
-        ((eq item (car lst)) t)
-        (t (member-eq item (cdr lst)))))
+        ((eq i (car lst)) t)
+        (t (mb i (cdr lst)))))
 
 
-(defun count-simple-paths (current end graph path)
-  (cond ((eq current end) 1)
-        (t (let ((neighbours (neighbour2 current graph)))
-             (reduce #'+ 
-                     (mapcar #'(lambda (next)
-                                 (if (member-eq next path)
-                                     0
-                                     (count-simple-paths next end graph (cons next path))))
-                             neighbours)
-                     :initial-value 0)))))
+(defun countpaths (start end graf path)
+  (cond ((eq start end) 1)
+        (t (let ((neighbours (neighb start graf)))
+          (reduce #'+ 
+            (mapcar #'(lambda (next)
+              (if (mb next path)
+                0
+                (countpaths next end graf (cons next path))))
+            neighbours)
+          :initial-value 0)))))
 
 
-(defun get-all-vertices (graph)
+(defun all (graph)
   (labels ((collect (g verts)
-             (cond ((null g) verts)
-                   (t (collect (cdr g)
-                               (add-if-new (add-if-new verts (caar g))
-                                           (cdar g))))))
-           (add-if-new (verts v)
-             (cond ((member-eq v verts) verts)
-                   (t (cons v verts)))))
-    (collect graph nil)))
+    (cond ((null g) verts)
+      (t (collect (cdr g)
+        (add-if-new (add-if-new verts (caar g))
+          (cdar g))))))
+        (add-if-new (verts v)
+        (cond ((mb v verts) verts)
+          (t (cons v verts)))))
+  (collect graph nil)))
 
 
 
-(defun connectivity-degree (graph)
-  (let ((vertices (get-all-vertices graph)))
+(defun f1 (graph)
+  (let ((vertices (all graph)))
     (cond ((or (null vertices) (null (cdr vertices))) 0)
-          (t
-           (let ((all-path-counts
-                   (mapcan #'(lambda (u)
-                               (mapcar #'(lambda (v)
-                                           (if (eq u v)
-                                               999999          ; пропускаем u == v
-                                               (count-simple-paths u v graph (list u))))
-                                       vertices))
-                           vertices)))
-             (reduce #'min all-path-counts :initial-value 999999))))))
+      (t
+        (let ((counts
+          (mapcan #'(lambda (u)
+            (mapcar #'(lambda (v)
+              (if (eq u v)
+                999999 
+                (countpaths u v graph (list u))))
+            vertices))
+            vertices)))
+  (let ((result (remove 0 counts)))
+    (if (null result)
+        0
+        (reduce #'min result :initial-value 999999))))))))
 
 
 (let ((g '((1 . 2) (1 . 3) (2 . 3) (3 . 1) (3 . 5) (5 . 1))))
   (print "Степень связности орграфа:")
-  (print (connectivity-degree g)))
+  (print (f1 g)))
 
 (let ((g '((1 . 2) (1 . 3) (2 . 3) (3 . 2) (2 . 1) (3 . 1))))
   (print "Степень связности орграфа:")
-  (print (connectivity-degree g)))
+  (print (f1 g)))
+
+(let ((g '((1 . 2) (2 . 3))))
+  (print "Степень связности орграфа:")
+  (print (f1 g)))
+
+(let ((g '((1 . 2) (1 . 3) (2 . 4) (3 . 4))))
+  (print "Степень связности орграфа:")
+  (print (f1 g)))
+
+(let ((g '((1 . 2) (2 . 3) (3 . 1))))
+  (print "Степень связности орграфа:")
+  (print (f1 g)))
+
+(let ((g '((1 . 2) (1 . 3) (2 . 1) (2 . 3) (3 . 1) (3 . 2))))
+  (print "Степень связности орграфа:")
+  (print (f1 g)))
+
+(let ((g '((1 . 2) (3 . 4))))
+  (print "Степень связности орграфа:")
+  (print (f1 g)))
