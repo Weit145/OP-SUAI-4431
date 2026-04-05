8e96e2a09e6bf7244eb80ef3175e7fa7dfc61374
Weit145 <weit.dima@yandex.ru>
2026-04-05 18:33:24 +0300

    TODO :memo:

---
+++ 2 curs/2_half/AL_kurs/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/weit/Документы/OP-SUAI-4431/2 curs/2_half/AL_kurs/domain/domain.cpp" "CMakeFiles/main.dir/domain/domain.cpp.o" "gcc" "CMakeFiles/main.dir/domain/domain.cpp.o.d"
+  "/home/weit/Документы/OP-SUAI-4431/2 curs/2_half/AL_kurs/hash/hash.cpp" "CMakeFiles/main.dir/hash/hash.cpp.o" "gcc" "CMakeFiles/main.dir/hash/hash.cpp.o.d"
+  "/home/weit/Документы/OP-SUAI-4431/2 curs/2_half/AL_kurs/main.cpp" "CMakeFiles/main.dir/main.cpp.o" "gcc" "CMakeFiles/main.dir/main.cpp.o.d"
+  "/home/weit/Документы/OP-SUAI-4431/2 curs/2_half/AL_kurs/transaction/transaction.cpp" "CMakeFiles/main.dir/transaction/transaction.cpp.o" "gcc" "CMakeFiles/main.dir/transaction/transaction.cpp.o.d"
+  "/home/weit/Документы/OP-SUAI-4431/2 curs/2_half/AL_kurs/tree/tree.cpp" "CMakeFiles/main.dir/tree/tree.cpp.o" "gcc" "CMakeFiles/main.dir/tree/tree.cpp.o.d"
+  "/home/weit/Документы/OP-SUAI-4431/2 curs/2_half/AL_kurs/variant/variant.cpp" "CMakeFiles/main.dir/variant/variant.cpp.o" "gcc" "CMakeFiles/main.dir/variant/variant.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 2 curs/2_half/AL_kurs/build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/domain/domain.cpp.o"
+  "CMakeFiles/main.dir/domain/domain.cpp.o.d"
+  "CMakeFiles/main.dir/hash/hash.cpp.o"
+  "CMakeFiles/main.dir/hash/hash.cpp.o.d"
+  "CMakeFiles/main.dir/main.cpp.o"
+  "CMakeFiles/main.dir/main.cpp.o.d"
+  "CMakeFiles/main.dir/transaction/transaction.cpp.o"
+  "CMakeFiles/main.dir/transaction/transaction.cpp.o.d"
+  "CMakeFiles/main.dir/tree/tree.cpp.o"
+  "CMakeFiles/main.dir/tree/tree.cpp.o.d"
+  "CMakeFiles/main.dir/variant/variant.cpp.o"
+  "CMakeFiles/main.dir/variant/variant.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
